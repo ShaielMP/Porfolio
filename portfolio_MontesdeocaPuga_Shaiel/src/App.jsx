641848c717b95ef151ec209509f3852f8e695951
ShaielMP <shaielmp0@gmail.com>
2025-10-01 18:02:40 +0200

    Deleted LightRay and added navbar

--- portfolio_MontesdeocaPuga_Shaiel/src/App.jsx
+++ portfolio_MontesdeocaPuga_Shaiel/src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import LightRays from "./components/LightRays";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="light-rays-container"  style={{ width: '100%', height: '100%', position: 'relative' }}>
+        <LightRays
+          raysOrigin="top-center"
+          raysColor="#ffffffff"
+          raysSpeed={0.6}
+          lightSpread={0.8}
+          rayLength={3}
+          followMouse={false}
+          mouseInfluence={0}
+          noiseAmount={0.1}
+          distortion={0.05}
+          className="custom-rays"
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
