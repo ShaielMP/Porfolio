068e97f5bfa4589363ceb82f68cecba877be8bcf
ShaielMP <shaielmp0@gmail.com>
2025-10-04 22:11:00 +0200

    added logo-carousel and fixed timeline-dot position

--- porfolio_MontesdeocaPuga_Shaiel/src/App.jsx
+++ porfolio_MontesdeocaPuga_Shaiel/src/App.jsx
@@ -21,14 +21,13 @@ function App() {
                 funcionales y atractivas. Especializado en frontend con React y
                 siempre aprendiendo nuevas tecnologías.
               </span>
-              <span className="subtitle-line"></span>
             </div>
           </div>
 
           <div className="hero-info">
             <div className="hero-profile-pic">
               <img
-                src="/public/images/shaiel.webp"
+                src="/images/shaiel.webp"
                 alt="Shaiel Montesdeoca Puga profile picture"
               />
             </div>
@@ -47,10 +46,97 @@ function App() {
           </div>
         </div>
 
-        <div className="scroll-indicator">
-          <div className="scroll-circle">
-            <span>Scroll</span>
-            <span>down</span>
+        <div className="tech-carousel">
+          <div className="carousel-track">
+            <div className="tech-item">
+              <i className="devicon-html5-plain colored"></i>
+              <span>HTML5</span>
+            </div>
+            <div className="tech-item">
+              <i className="devicon-css3-plain colored"></i>
+              <span>CSS3</span>
+            </div>
+            <div className="tech-item">
+              <i className="devicon-javascript-plain colored"></i>
+              <span>JavaScript</span>
+            </div>
+            <div className="tech-item">
+              <i className="devicon-react-original colored"></i>
+              <span>React</span>
+            </div>
+            <div className="tech-item">
+              <i className="devicon-nodejs-plain colored"></i>
+              <span>Node.js</span>
+            </div>
+            <div className="tech-item">
+              <i className="devicon-git-plain colored"></i>
+              <span>Git</span>
+            </div>
+            <div className="tech-item">
+              <i className="devicon-github-original"></i>
+              <span>GitHub</span>
+            </div>
+            <div className="tech-item">
+              <i className="devicon-mysql-plain colored"></i>
+              <span>MySQL</span>
+            </div>
+            <div className="tech-item">
+              <i className="devicon-postgresql-plain colored"></i>
+              <span>PostgreSQL</span>
+            </div>
+            <div className="tech-item">
+              <i className="devicon-php-plain colored"></i>
+              <span>PHP</span>
+            </div>
+            <div className="tech-item">
+              <i className="devicon-java-plain colored"></i>
+              <span>Java</span>
+            </div>
+
+            <div className="tech-item">
+              <i className="devicon-html5-plain colored"></i>
+              <span>HTML5</span>
+            </div>
+            <div className="tech-item">
+              <i className="devicon-css3-plain colored"></i>
+              <span>CSS3</span>
+            </div>
+            <div className="tech-item">
+              <i className="devicon-javascript-plain colored"></i>
+              <span>JavaScript</span>
+            </div>
+            <div className="tech-item">
+              <i className="devicon-react-original colored"></i>
+              <span>React</span>
+            </div>
+            <div className="tech-item">
+              <i className="devicon-nodejs-plain colored"></i>
+              <span>Node.js</span>
+            </div>
+            <div className="tech-item">
+              <i className="devicon-git-plain colored"></i>
+              <span>Git</span>
+            </div>
+            <div className="tech-item">
+              <i className="devicon-github-original"></i>
+              <span>GitHub</span>
+            </div>
+            <div className="tech-item">
+              <i className="devicon-mysql-plain colored"></i>
+              <span>MySQL</span>
+            </div>
+            <div className="tech-item">
+              <i className="devicon-postgresql-plain colored"></i>
+              <span>PostgreSQL</span>
+            </div>
+            <div className="tech-item">
+              <i className="devicon-php-plain colored"></i>
+              <span>PHP</span>
+            </div>
+            <div className="tech-item">
+              <i className="devicon-java-plain colored"></i>
+              <span>Java</span>
+            </div>
           </div>
         </div>
       </section>
