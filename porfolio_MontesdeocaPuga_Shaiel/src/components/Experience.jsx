46e8926d99ff34f7cde1844e5c063271d91e3ee3
ShaielMP <shaielmp0@gmail.com>
2025-10-10 00:47:51 +0200

    changed tech-carousel and experience section

--- porfolio_MontesdeocaPuga_Shaiel/src/components/Experience.jsx
+++ porfolio_MontesdeocaPuga_Shaiel/src/components/Experience.jsx
@@ -1,10 +1,18 @@
 export function Experience() {
-  const education = [
+  const experience = [
+    {
+      year: "Marzo 2025 - Mayo 2025",
+      title: "Web Developer Internship",
+      institution: "John Weston Group",
+      description:
+        "Desarrollo de habilidades y conocimientos en desarrollo web. Aquí pude colaborar con el equipo de desarrollo, aprender nuevas herramientas y mejorar mis habilidades como desarrollador web.",
+      type: "work",
+    },
     {
       year: "2023 - Junio 2025",
       title: "Ciclo Superior de Desarrollo de Aplicaciones Web",
       institution: "IES Ana Luisa Benitez",
-      description: "Desarrollo de aplicaciones web Frontend y Backend.",
+      description: "Desarrollo de aplicaciones web Frontend y Backend. Donde aprendí a trabajar con tecnologías como Java, PHP, JavaScript, HTML y CSS, MySQL, PostgreSQL y Git. ",
       type: "education",
     },
     {
@@ -12,30 +20,23 @@ export function Experience() {
       title: "Grado en Ingeniería Informática",
       institution: "ULPGC",
       description:
-        "Dos años de estudios en Ingeniería Informática, con bases en programación, algoritmos, bases de datos y sistemas.",
+        "Dos años de estudios en Ingeniería Informática, con bases en programación, algoritmos, bases de datos y sistemas. No llegué a terminar el grado, pero dí mis primeros pasos en el mundo de la programación y gracias a ello he podido aprender muchas cosas que me han ayudado a crecer como desarrollador.",
       type: "education",
-    },
-  ];
-
-  const work = [
-    {
-      year: "Marzo 2025 - Mayo 2025",
-      title: "Web Developer Internship",
-      institution: "John Weston Group",
-      description:
-        "Desarrollo de habilidades y conocimientos en desarrollo web.",
-      type: "work",
-    },
+    }
   ];
 
   const TimelineItem = ({ item }) => (
     <div className="timeline-item">
       <div className="timeline-dot"></div>
       <div className="timeline-content">
-        <span className="timeline-year">{item.year}</span>
-        <h3 className="timeline-title">{item.title}</h3>
-        <h4 className="timeline-institution">{item.institution}</h4>
-        <p className="timeline-description">{item.description}</p>
+        <div className="timeline-header">
+          <h3 className="timeline-title">{item.title}</h3>
+          <h4 className="timeline-institution">{item.institution}</h4>
+          <p className="timeline-year">{item.year}</p>
+        </div>
+        <div className="timeline-description-container">
+          <p className="timeline-description">{item.description}</p>
+        </div>
       </div>
     </div>
   );
@@ -50,60 +51,8 @@ export function Experience() {
 
         <div className="experience-content">
           <div className="experience-column">
-            <div className="column-header">
-              <div className="header-icon">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="#3b82f6"
-                  stroke-width="1.5"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  class="icon icon-tabler icons-tabler-outline icon-tabler-school"
-                >
-                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                  <path d="M22 9l-10 -4l-10 4l10 4l10 -4v6" />
-                  <path d="M6 10.6v5.4a6 3 0 0 0 12 0v-5.4" />
-                </svg>
-              </div>
-              <h3>Educación</h3>
-            </div>
             <div className="timeline">
-              {education.map((item, index) => (
-                <TimelineItem key={index} item={item} />
-              ))}
-            </div>
-          </div>
-
-          <div className="experience-column">
-            <div className="column-header">
-              <div className="header-icon">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="#3b82f6"
-                  stroke-width="1.5"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  class="icon icon-tabler icons-tabler-outline icon-tabler-briefcase"
-                >
-                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                  <path d="M3 7m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v9a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z" />
-                  <path d="M8 7v-2a2 2 0 0 1 2 -2h4a2 2 0 0 1 2 2v2" />
-                  <path d="M12 12l0 .01" />
-                  <path d="M3 13a20 20 0 0 0 18 0" />
-                </svg>
-              </div>
-              <h3>Experiencia Laboral</h3>
-            </div>
-            <div className="timeline">
-              {work.map((item, index) => (
+              {experience.map((item, index) => (
                 <TimelineItem key={index} item={item} />
               ))}
             </div>
